4a25abb3d53e8cf1257f7376bc080934f538bb4d
Andrey Alexandre <andreyalexandre16@gmail.com>
2021-12-18 17:42:03 -0300

    Adicionado postgres

--- receitasProjeto/receitas/views.py
+++ receitasProjeto/receitas/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    recipe_data = {
+        1: 'Lasanha',
+        2: 'Sorvete',
+        3: 'Bolo',
+        4: 'Bolo de pote'
+    }
+    return render(request, 'index.html', context={'recipe_dict': recipe_data})
 
 
 def receita(request):
