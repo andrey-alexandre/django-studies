275ac2e07f5816cd782c57c8e79dace8b67c3842
Andrey Alexandre <andreyalexandre16@gmail.com>
2021-12-18 11:02:15 -0300

    Iniciados estudos

---
+++ receitasProjeto/receitas/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 08:24:35 2021
+
+@author: Andrey
+"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('receita/', views.receita, name='receita')
+    ]
